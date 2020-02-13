874eecbc84cebb21640e6eb9fd93fc1e264ac4e6
jermy.mthangani <jermy.mthangani@gmail.com>
2020-02-13 15:44:19 +0300

    Website page

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var saySomething= function(whatToSay) {
+    alert(whatToSay)
+}
+saySomething("Hello! Welcome to my webpage.")
+var DayOfTheWeek= function(CC,YY,MM,DD) {
+    return ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )
+}
+var CC=parseInt(prompt("Enter the century you were born"));
+var YY=parseInt(prompt("Enter the year you were born"));
+var MM=parseInt(prompt("Enter the month you were born "));
+var DD=parseInt(prompt("Enter the day "));
+var result=DayOfTheWeek(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ));
+alert(result);
