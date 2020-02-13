35104499f38bf78f16109990ec0d16d0befbe9fe
jermy.mthangani <jermy.mthangani@gmail.com>
2020-02-13 15:58:48 +0300

    Added modulous

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ var saySomething= function(whatToSay) {
 }
 saySomething("Hello! Welcome to my webpage.")
 var DayOfTheWeek= function(CC,YY,MM,DD) {
-    return ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )
+    return ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7;
 }
 var CC=parseInt(prompt("Enter the century you were born"));
 var YY=parseInt(prompt("Enter the year you were born"));
