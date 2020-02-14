59e0f05147c47c88cadbe6479b9fdaf07222189c
jermy.mthangani <jermy.mthangani@gmail.com>
2020-02-14 12:44:28 +0300

    Added javascript variant weekdays

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,24 @@
-var saySomething= function(whatToSay) {
-    alert(whatToSay)
-}
-saySomething("Hello! Welcome to my webpage.")
-var DayOfTheWeek= function(CC,YY,MM,DD) {
-    return ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7;
-}
-var CC=parseInt(prompt("Enter the century you were born"));
-var YY=parseInt(prompt("Enter the year you were born"));
-var MM=parseInt(prompt("Enter the month you were born "));
-var DD=parseInt(prompt("Enter the day "));
-var result=DayOfTheWeek(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ));
-alert(result);
+var day= function(CC,YY,MM,DD) {
+    return  ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
+    }
+
+var CC=parseInt(prompt("Century"));
+var YY=parseInt(prompt("Year"));
+var MM=parseInt(prompt("Month"));
+var DD=parseInt(prompt("Day"));
+
+var weekday= day(CC,YY,MM,DD);
+var days=weekday.toFixed(0);
+alert(days);
+
+
+  var myDays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+     var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaki","Yaw","Kofi","Kwame"];
+     var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
+    
+if (days==0){
+    alert(maleNames[0]);}
+    else{
+    alert("enter a valid date");
+    }
