c7233f487cee660ffabb0469a052b20d2513d0d7
jermy.mthangani <jermy.mthangani@gmail.com>
2020-02-17 09:34:57 +0300

    Removed Second container blocking the texts aligned

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function akanNames(){
-    var year=document.getElementById("year").value;
+    var year= document.getElementById("year").value;
     var MM = parseInt(document.getElementById("month").value);
     var DD = parseInt(document.getElementVyId("date").value);
     var CC = parseInt( year.slice(0,2));
@@ -12,21 +12,21 @@ function akanNames(){
 
 
     var CalculateDate = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )% 7;
-    CalculateDate= Math.floor(CalculateDate)
+    CalculateDate= Math.floor(CalculateDate);
 
     if( MM<1||MM>12){
         alert("please fill in the correct month")
-    };
+    }
     if(DD>31 || DD<1){
         alert("please fill in the correct date")
-    };
+    }
 
     if (gender ==="male"){
         sex=male;
     }
     else{
         sex = female;
-    };
+    }
 
     var output= sex[CalculateDate]
     document.getElementById("answer").innerHTML ="Congratulations your Akan name is"+ output+".";
