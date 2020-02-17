ef580abee65bbab0abbbe50005ece1393095c4df
jermy.mthangani <jermy.mthangani@gmail.com>
2020-02-17 06:44:51 +0300

    Added label input for year,month,Date and gender

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,36 @@
-var day= function(CC,YY,MM,DD) {
-    return  ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
+function akanNames(){
+    var year=document.getElementById("year").value);
+    var MM = parseInt(document.getElementById("month").value);
+    var DD = parseInt(document.getElementVyId("date").value);
+    var CC = parseInt( year.slice(0,2));
+    var YY =parseInt(year.slice(2,4));
+
+    var male =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var female= ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua"];
+    var gender =document.getElementById("gender").value;
+    console.log(gender);
+
+
+    var CalculateDate = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )% 7;
+    CalculateDate= Math.floor(CalculateDate)
+
+    if( MM<1||MM>12){
+        alert("please fill in the correct month")
+    };
+    if(DD>31 || DD<1){
+        alert("please fill in the correct date")
+    };
+
+    if (gender) ==="male"){
+        sex=male;
     }
+    else{
+        sex = female;
+    };
 
-var CC=parseInt(prompt("Century"));
-var YY=parseInt(prompt("Year"));
-var MM=parseInt(prompt("Month"));
-var DD=parseInt(prompt("Day"));
+    var output= sex[CalculateDate]
+    document.getElementById("answer").innerHTML ="Congratulations your Akan name is"+ output+".";
 
-var weekday= day(CC,YY,MM,DD);
-var days=weekday.toFixed(0);
-alert(days);
-
-
-     var myDays=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-     var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaki","Yaw","Kofi","Kwame"];
-     var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-
-     if (gender==='male'){
-        alert(maleNames[days]);
-        } else if (gender==='female') {
-        alert(femaleNames[days]);
-        }
+};
         
         
